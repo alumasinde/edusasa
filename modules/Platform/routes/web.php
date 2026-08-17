e640c6019b3df2801dbf92bfb50516a1e60d5f5d
alumasinde <143938688+alumasinde@users.noreply.github.com>
2026-08-17 21:36:34 +0300

    feat(platform): add entitlement administration routes

--- modules/Platform/routes/web.php
+++ modules/Platform/routes/web.php
@@ -8,11 +8,21 @@ use Modules\Platform\Controllers\PlatformController;
 
 /** @var Router $router */
 $router->group([
-    'prefix' => '/platform',
-    'middleware' => ['platform_host', 'platform_auth'],
-], function (Router $router): void {
-    $router->get('/', [PlatformController::class, 'dashboard']);
-    $router->get('/schools', [PlatformController::class, 'schoolPage']);
-    $router->get('/schools/new', [OnboardingController::class, 'create']);
-    $router->post('/schools', [OnboardingController::class, 'store']);
+    'prefix'=>'/platform',
+    'middleware'=>['platform_host','platform_auth'],
+],function(Router $router):void{
+    $router->get('/',[PlatformController::class,'dashboard']);
+    $router->get('/schools',[PlatformController::class,'schoolPage']);
+    $router->get('/schools/new',[OnboardingController::class,'create']);
+    $router->post('/schools',[OnboardingController::class,'store']);
+
+    $router->get('/entitlements',[PlatformController::class,'entitlementPage']);
+    $router->get('/plans/{id}',[PlatformController::class,'planPage']);
+    $router->post('/plans',[PlatformController::class,'savePlan']);
+    $router->post('/plans/{id}',[PlatformController::class,'savePlan']);
+    $router->post('/plans/{plan}/features/{feature}',[PlatformController::class,'setPlanFeature']);
+
+    $router->get('/schools/{id}/entitlements',[PlatformController::class,'schoolEntitlementsPage']);
+    $router->post('/schools/{id}/entitlements',[PlatformController::class,'saveSchoolOverride']);
+    $router->delete('/schools/{id}/entitlements',[PlatformController::class,'removeSchoolOverride']);
 });
