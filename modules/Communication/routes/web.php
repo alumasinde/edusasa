706d3d0885b07980e4a6e0e95509d8da6058df18
alumasinde <143938688+alumasinde@users.noreply.github.com>
2026-08-18 20:59:16 +0300

    Fix Phase 12 communication route permissions

--- modules/Communication/routes/web.php
+++ modules/Communication/routes/web.php
@@ -3,13 +3,13 @@
 declare(strict_types=1);
 /** @var \App\Core\Router $router */
 use Modules\Communication\Controllers\CommunicationController;
-$router->group(['prefix'=>'/communication','middleware'=>['tenant','auth','module:communication','permission:communication.view,communication.inbox']],function($router){
+$router->group(['prefix'=>'/communication','middleware'=>['tenant','auth','module:communication']],function($router){
  $router->get('',[CommunicationController::class,'index'],['permission:communication.view']);
  $router->get('/create',[CommunicationController::class,'create'],['permission:communication.manage']);
  $router->post('',[CommunicationController::class,'store'],['csrf','permission:communication.manage']);
  $router->get('/inbox',[CommunicationController::class,'inbox'],['permission:communication.inbox']);
  $router->post('/{id}/read',[CommunicationController::class,'read'],['csrf','permission:communication.inbox']);
- $router->get('/{id}',[CommunicationController::class,'show']);
+ $router->get('/{id}',[CommunicationController::class,'show'],['permission:communication.view']);
  $router->post('/{id}/publish',[CommunicationController::class,'publish'],['csrf','permission:communication.send']);
  $router->post('/{id}/archive',[CommunicationController::class,'archive'],['csrf','permission:communication.manage']);
 });
