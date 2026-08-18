126feb99fae0e6f0943da3a80e40be9eb1fe2e91
alumasinde <143938688+alumasinde@users.noreply.github.com>
2026-08-18 13:40:26 +0300

    feat(exams): wire paper configuration routes

--- modules/Exams/routes/web.php
+++ modules/Exams/routes/web.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 /** @var \App\Core\Router $router */
 
 use Modules\Exams\Controllers\ExaminationController;
+use Modules\Exams\Controllers\ExaminationPaperController;
 
 $router->group([
     'prefix'=>'/exams',
@@ -13,6 +14,10 @@ $router->group([
     $router->get('',[ExaminationController::class,'index']);
     $router->get('/create',[ExaminationController::class,'create'],['permission:exams.manage']);
     $router->post('',[ExaminationController::class,'store'],['csrf','permission:exams.manage']);
+    $router->get('/{id}/papers',[ExaminationPaperController::class,'index'],['permission:exams.papers.view,exams.papers.manage']);
+    $router->post('/{id}/papers',[ExaminationPaperController::class,'store'],['csrf','permission:exams.papers.manage']);
+    $router->post('/{id}/papers/status',[ExaminationPaperController::class,'status'],['csrf','permission:exams.papers.manage']);
+    $router->post('/{id}/papers/delete',[ExaminationPaperController::class,'delete'],['csrf','permission:exams.papers.manage']);
     $router->get('/{id}',[ExaminationController::class,'show']);
     $router->post('/{id}/status',[ExaminationController::class,'status'],['csrf','permission:exams.manage']);
 });
